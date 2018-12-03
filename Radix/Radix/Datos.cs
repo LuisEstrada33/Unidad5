e11e7574b805e408b22a261139409c996c989cab
LuisEstrada33 <43280434+LuisEstrada33@users.noreply.github.com>
2018-12-03 08:33:25 -0800

    Add files via upload

---
+++ Radix/Radix/Datos.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radix
+{
+    class Datos
+    {
+        Queue<int> values = new Queue<int>(); //Cola que va almacenando los valores ingresados
+        public void Entervalues()
+        {
+            int[] Array; //Arreglo que será enviado como parámetro
+            bool continuee = true;
+            while (continuee) //Mientras la condición sea verdadera
+            {
+                try
+                {
+                    Console.Clear();
+                    Console.Write("Opciones disponibles: \n1.- Ingresar valor \n2.- Ordenar \n3.- Predefinidos \n4.- Salir \nOpción deseada: "); //Opciones disponibles
+                    switch (Convert.ToInt16(Console.ReadLine()))
+                    {
+                        case 1:
+                            Console.Clear();
+                            Console.Write("Ingrese valor: ");
+                            values.Enqueue(Convert.ToInt32(Console.ReadLine())); //Recibe el valor tecleado
+                            break;
+                        case 2:
+                            Array = new int[values.Count]; //Se inicializa el arreglo con la longitud de la cola
+                            for (int counter = 0; counter < values.Count; counter++) //Aquí se llena el arreglo con los valores de la cola
+                                Array[counter] = values.ElementAt(counter);
+                            Console.Clear();
+                            Sorting Ordenar = new Sorting(); //Se hace la instanciación de la clase 
+                            Ordenar.Ordenar(Array); //Se hace la llamada al método, se envia como parametro el arreglo
+                            values.Clear(); //Se limpia la cola
+                            Console.ReadKey();
+                            break;
+                        case 3:
+                            Console.Write("Los predefinidos disponibles son: \n1.- Conjunto 1 \n2.- Conjunto 2 \n3.- Conjunto 3 \n4.- Conjunto 4 \n5.- Conjunto 5 \nConjunto seleccionado: ");
+                            if (Predefined(Convert.ToInt32(Console.ReadLine())).Count != 0)
+                            {
+                                Array = new int[values.Count]; //Se inicializa el arreglo con la longitud de la cola
+                                for (int counter = 0; counter < values.Count; counter++) //Aquí se llena el arreglo con los valores de la cola
+                                    Array[counter] = values.ElementAt(counter);
+                                Console.Clear();
+                                Sorting Ordena = new Sorting(); //Se hace la instanciación de la clase 
+                                Ordena.Ordenar(Array); //Se hace la llamada al método, se envia como parametro el arreglo
+                                values.Clear(); //Se limpia la cola
+                                Console.ReadKey();
+                            }
+                            break;
+                        case 4:
+                            continuee = false;
+                            Console.Write("Bye"); //Si se indica la opción 3 se cierra el programa
+                            break;
+                        default: //De no ser una opción válida se despliega el mensaje y vuelve a iniciar
+                            Console.Clear();
+                            Console.WriteLine("Ingrese una opción válida");
+                            Console.ReadKey();
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message); //Si hay un error, se lanza el mensaje de la excepción
+                    Console.ReadKey();
+                }
+            }
+        }
+
+        public Queue<int> Predefined(int opcion) //Método que regresa como valor una cola
+        {
+            switch (opcion)
+            {
+                case 1:
+                    values.Enqueue(3);
+                    values.Enqueue(6);
+                    values.Enqueue(9);
+                    values.Enqueue(5);
+                    values.Enqueue(1);
+                    values.Enqueue(4);
+                    values.Enqueue(7);
+                    values.Enqueue(2);
+                    values.Enqueue(1);
+                    values.Enqueue(3);
+                    return values;
+                case 2:
+                    values.Enqueue(8);
+                    values.Enqueue(3);
+                    values.Enqueue(9);
+                    values.Enqueue(3);
+                    values.Enqueue(11);
+                    values.Enqueue(7);
+                    values.Enqueue(1);
+                    values.Enqueue(27);
+                    values.Enqueue(12);
+                    return values;
+                case 3:
+                    values.Enqueue(10);
+                    values.Enqueue(40);
+                    values.Enqueue(36);
+                    values.Enqueue(5);
+                    values.Enqueue(24);
+                    values.Enqueue(2);
+                    values.Enqueue(5);
+                    values.Enqueue(8);
+                    return values;
+                case 4:
+                    values.Enqueue(55);
+                    values.Enqueue(42);
+                    values.Enqueue(0);
+                    values.Enqueue(-3);
+                    values.Enqueue(0);
+                    values.Enqueue(-1);
+                    values.Enqueue(2);
+                    values.Enqueue(4);
+                    values.Enqueue(7);
+                    return values;
+                case 5:
+                    values.Enqueue(25);
+                    values.Enqueue(108);
+                    values.Enqueue(1024);
+                    values.Enqueue(12);
+                    values.Enqueue(351);
+                    values.Enqueue(251);
+                    values.Enqueue(39);
+                    return values;
+                default:
+                    Console.WriteLine("Opción no válida");
+                    return values;
+            }
+        }
+    }
+}
---
+++ Radix/Radix/Sorting.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radix
+{
+    class Sorting
+    {
+        public void Ordenar(int[] Temporal)
+        {
+            RadixSort(ref Temporal);
+            List<int> negative = new List<int>(); //Lista de valores negativos ordenados
+            List<int> array = new List<int>(); //Lista de valores ordenados positivos
+            foreach (var item in Temporal)
+            {
+                if (item < 0) //Si es menor que 0 se agrega a la lista de negativos
+                {
+                    negative.Add(item);
+
+                }
+                else //Si no es negativo se agrega a la lista de ordenados positivos
+                    array.Add(item);
+            }
+            foreach (var item in negative)
+                Console.Write(item + " "); //Se imprimen los valores acomodados
+            foreach (var item in array)
+                Console.Write(item + " ");
+        }
+
+        private void RadixSort(ref int[] a) //Arreglo como ref para modificarlo en otro método
+        {
+            // Arreglo temporal 
+            int[] temp = new int[a.Length];
+
+            // Número de bits de la longitud del grupo
+            int r = 4; // Se puede usar como 2, 8 o 16 para comprobar si es mas rápido 
+
+            // La cantidad de bits del tipo int
+            int b = 32;
+
+            // Conteo y prefijo del arreglo (o cifra significativa)
+            int[] count = new int[1 << r];
+            int[] significativa = new int[1 << r];
+
+            // Número de grupos
+            int groups = (int)Math.Ceiling(b / (double)r);
+
+            // Mascara para identificar los grupos 
+            int mask = (1 << r) - 1;
+
+            // Empieza el algoritmo 
+            for (int c = 0, shift = 0; c < groups; c++, shift += r)
+            {
+                // Se reinicia el conteo del array
+                for (int j = 0; j < count.Length; j++)
+                    count[j] = 0;
+
+                // Cuenta los elementos del grupo de posición c
+                for (int i = 0; i < a.Length; i++)
+                    count[(a[i] >> shift) & mask]++;
+
+                // Cálculo de prefijo o cifra significativa
+                significativa[0] = 0;
+                for (int i = 1; i < count.Length; i++)
+                    significativa[i] = significativa[i - 1] + count[i - 1];
+
+                // desde a[] hasta t[] elementos ordenados por el grupo de posición c
+                for (int i = 0; i < a.Length; i++)
+                    temp[significativa[(a[i] >> shift) & mask]++] = a[i];
+
+                // a[]=t[] y empieza otra vez hasta el último grupo
+                temp.CopyTo(a, 0);
+            }
+            //El arreglo ahora está ordenado
+        }
+    }
+}
